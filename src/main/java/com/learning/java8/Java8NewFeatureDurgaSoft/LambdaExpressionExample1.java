f8a2da14170840d19ed2e50fa3293f7294459225
rahulmendiratta13@gmail.com <rahulmendiratta13@gmail.com>
2020-04-12 18:19:32 +0530

    Java8 New featur

---
+++ src/main/java/com/learning/java8/Java8NewFeatureDurgaSoft/LambdaExpressionExample1.java
@@ -0,0 +1,23 @@
+package com.learning.java8.Java8NewFeatureDurgaSoft;
+
+
+@FunctionalInterface
+interface Interf1 {
+
+    void getSum(int i, int j);
+
+}
+
+
+public class LambdaExpressionExample1{
+
+    public static void main(String... args){
+
+        Interf1 interf1 = (a,b) -> System.out.println(a+b);
+        interf1.getSum(30,60);
+
+    }
+
+
+
+}
