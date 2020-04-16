312e93c4791682897a38bdfeea6342c380ab677d
rahulmendiratta13@gmail.com <rahulmendiratta13@gmail.com>
2020-04-16 22:39:31 +0530

    Comparator Example

--- src/main/java/com/learning/java8/Java8NewFeatureDurgaSoft/LambdaExpressionComparatorExample1.java
+++ src/main/java/com/learning/java8/Java8NewFeatureDurgaSoft/LambdaExpressionComparatorExample1.java
@@ -1,27 +1,23 @@
 package com.learning.java8.Java8NewFeatureDurgaSoft;
 
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.TreeSet;
 
 public class LambdaExpressionComparatorExample1 {
 
     public static void main(String[] args) {
-        TreeSet<Integer> treeSet = new TreeSet<>((o1,o2) ->{
-            Integer i1 = (Integer)o1;
-            Integer i2 = (Integer)o2;
-            if(i1>i2){
-                return +1;
-            }else if(i1<i2){
-                return -1;
-            }else{
-                return 0;
-            }
+        TreeSet<Integer> treeSet = new TreeSet<>((o1,o2) -> {
+            Integer i1 = (Integer) o1;
+            Integer i2 = (Integer) o2;
+            return (i1 > i2) ? -1 : (i1 < i2) ? +1 : 0;
         });
         treeSet.add(45);
         treeSet.add(12);
         treeSet.add(65);
         System.out.println(treeSet);
+
     }
 
 }
