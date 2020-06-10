632802bedc68aa6d1868e118ac529ca207450ddf
rahulmendiratta13@gmail.com <rahulmendiratta13@gmail.com>
2020-06-11 01:17:02 +0530

    ThreadPool Example 1

---
+++ src/main/java/com/learning/java8/Java8NewFeatureDurgaSoft/AdvanceMultiThreading/RenterrentLockExample4.java
@@ -0,0 +1,39 @@
+package com.learning.java8.Java8NewFeatureDurgaSoft.AdvanceMultiThreading;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class RenterrentLockExample4 {
+
+    public static void main(String[] args){
+
+        ReentrantLock reentrantLock = new ReentrantLock();
+
+        Runnable r = ()-> {
+            try {
+                if (reentrantLock.tryLock(1000, TimeUnit.MILLISECONDS)) {
+                    System.out.println(Thread.currentThread().getName() + " got a lock");
+                    try {
+                        Thread.sleep(5000);
+                    } catch (InterruptedException nsee) {
+                        nsee.printStackTrace();
+                    }
+                    reentrantLock.unlock();
+                } else {
+                    System.out.println(Thread.currentThread().getName() + " didn't get a lock");
+                }
+            }catch (InterruptedException nsee){
+                nsee.printStackTrace();
+            }
+        };
+
+        Thread thread1 = new Thread(r,"First Thread");
+        Thread thread2 = new Thread(r,"Second Thread");
+        thread1.start();
+        thread2.start();
+
+
+
+
+    }
+}
---
+++ src/main/java/com/learning/java8/Java8NewFeatureDurgaSoft/AdvanceMultiThreading/ThreadPoolExample1.java
@@ -0,0 +1,33 @@
+package com.learning.java8.Java8NewFeatureDurgaSoft.AdvanceMultiThreading;
+
+import org.apache.commons.lang3.RandomUtils;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ThreadPoolExample1 {
+
+
+    public static void main(String[] args) {
+
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+
+        String[] names = {"Dharmo","Ramesh","Rahul","Hemant","Liyan","Jyoti","Yamini"};
+
+        Runnable r =()->{
+            System.out.println(Thread.currentThread().getName()+" Executing for "+ names[(int)(Math.random()*names.length)]);
+            try{
+                Thread.sleep(1000);
+            }catch(InterruptedException nsee){
+                nsee.printStackTrace();
+            }
+            System.out.println("Run method Executed");
+        };
+
+
+        for(String name:names){
+            executorService.submit(r);
+        }
+        executorService.shutdown();
+    }
+}
